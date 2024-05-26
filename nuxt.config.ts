b2ad9649b0ade2c765d61a3bfd03dd30080ab0f6
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-05-26 11:29:13 +0900

    🔨 : shadcn-vueの設定

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -5,5 +5,10 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: 'src/',
   ssr: true,
-  modules: ['@nuxt/eslint'],
+  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
+  // @ts-expect-error
+  shadcn: {
+    prefix: '',
+    componentDir: './src/components/ui/',
+  },
 });
