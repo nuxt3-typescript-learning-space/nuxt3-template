de18964f96624849d3f6fe1146fd41c6058efa04
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-05-29 21:00:47 +0900

    ✨ : piniaを使用したサンプルコンポーネント実装

---
+++ src/components/CounterDisplay.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+defineProps({
+  count: {
+    type: Number,
+    required: true,
+  },
+});
+</script>
+<template>
+  <div>counter: {{ count }}</div>
+</template>
--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,10 +1,17 @@
 <script setup lang="ts">
+import CounterDisplay from '@/components/CounterDisplay.vue';
 import Title from '@/components/Title.vue';
 import Button from '@/components/ui/button/Button.vue';
+import { useCounterStore } from '@/store/counterStore';
+const counterStore = useCounterStore();
+const { count: countState } = storeToRefs(counterStore);
+const { increment, decrement } = counterStore;
 </script>
 <template>
-  <div>
+  <section>
     <Title />
-    <Button>クリック</Button>
-  </div>
+    <Button @click="increment">Increment</Button>
+    <Button @click="decrement">Decrement</Button>
+    <CounterDisplay :count="countState" />
+  </section>
 </template>
