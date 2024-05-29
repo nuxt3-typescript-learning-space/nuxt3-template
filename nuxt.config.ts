90fd7e32a71fc8997c0e350afce265e214ab7621
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-05-29 20:27:30 +0900

    📦 : 不要なパッケージ削除

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,9 @@
-import { defineNuxtConfig } from 'nuxt/config';
-
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: 'src/',
   ssr: true,
   modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt'],
-  // @ts-expect-error
   shadcn: {
     prefix: '',
     componentDir: './src/components/ui/',
