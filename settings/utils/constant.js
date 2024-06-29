2bc6b7427765cdc909bf0cc2fb365afdc228d031
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-06-30 01:49:44 +0900

    :art: improve: 処理をutils関数に分割

---
+++ settings/utils/constant.js
@@ -0,0 +1,23 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+export const __filename = fileURLToPath(import.meta.url);
+export const __dirname = path.dirname(__filename);
+export const PROJECT_ROOT = path.resolve(__dirname, '..');
+export const STORE_DIR = path.resolve(PROJECT_ROOT, '../src/store');
+export const STORE_STATE_LIST_PATH = path.resolve(PROJECT_ROOT, '../settings/rules/data/store-state-list.json');
+/**
+ * 正規表現パターンの定義
+ * この正規表現は、以下のようなstateオブジェクトの定義をキャプチャするためのものです:
+ * state: (): State => ({
+ *   foo: 'bar',
+ *   baz: 'qux',
+ * })
+ * または
+ * state: () => ({
+ *   foo: 'bar',
+ *   baz: 'qux',
+ * })
+ */
+export const STATE_REGEX =
+  /state\s*:\s*\(\)\s*:\s*\w+\s*=>\s*\(\s*{([\s\S]*?)}\s*\)|state\s*:\s*\(\)\s*=>\s*\({([\s\S]*?)}\)/;
