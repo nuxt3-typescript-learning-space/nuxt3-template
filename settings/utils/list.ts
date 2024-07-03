83b6aac084a48b97db2f4fd0066e78c60d089f1a
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-07-03 20:18:10 +0900

    :art: improve: listデータを空にするため、不要になった処理を削除

--- settings/utils/list.ts
+++ settings/utils/list.ts
@@ -1,11 +1,10 @@
 /**
  * リストを更新する関数
- * @param {string[]} existingList - 既存のリスト
  * @param {string[]} newValues - 新しい値のリスト
  * @returns {string[]} - 更新されたリスト
  */
-export const updateList = (existingList: string[], newValues: string[]): string[] => {
-  const updatedList = existingList.filter((value) => newValues.includes(value));
+export const updateList = (newValues: string[]): string[] => {
+  const updatedList: string[] = [];
   newValues.forEach((value) => {
     if (!updatedList.includes(value)) {
       updatedList.push(value);
