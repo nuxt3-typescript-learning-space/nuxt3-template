f8b3f162b8bd22db8bebde481678ccebdbdefc8c
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-07-06 23:35:40 +0900

    :sparkles: feat: リアクティブな値に ".value" を付けることを強制するESLintルール

--- settings/rules/reactive-value-suffix.js
+++ settings/rules/reactive-value-suffix.js
@@ -1,177 +1,183 @@
-const reactiveFunctions = ['toRefs', 'storeToRefs', 'computed', 'ref', 'reactive'];
-
 /**
  * @fileoverview リアクティブな値に ".value" を付けることを強制するESLintルール
  */
+
+/**
+ * @typedef {import('eslint').Rule.RuleModule} RuleModule
+ * @typedef {import('eslint').Rule.RuleContext} RuleContext
+ * @typedef {import('estree').VariableDeclarator} VariableDeclarator
+ * @typedef {import('estree').Property} Property
+ * @typedef {import('estree').Identifier} Identifier
+ * @typedef {import('estree').FunctionDeclaration} FunctionDeclaration
+ * @typedef {import('estree').ArrowFunctionExpression} ArrowFunctionExpression
+ * @typedef {import('estree').MemberExpression} MemberExpression
+ * @typedef {import('estree').CallExpression} CallExpression
+ */
+
+/**
+ * ASTのノードがFunctionDeclarationまたはArrowFunctionExpressionの型かを確認し、引数のリストに追加する関数
+ * @param {FunctionDeclaration | ArrowFunctionExpression} node ASTのノード
+ * @param {string[]} list 引数のリスト
+ * @returns {void}
+ */
+function addArgumentsToList(node, list) {
+  if (node.params.length > 0) {
+    list.push(...node.params.map((param) => param.name).filter(Boolean));
+  }
+}
+
+/**
+ * ノードがreactiveFunctionsに含まれる変数かを確認し、リストに追加する関数
+ * @param {VariableDeclarator} node ASTのノード
+ * @param {string[]} list 変数名リスト
+ * @param {string[]} reactiveFunctions リアクティブな関数名のリスト
+ * @returns {void}
+ */
+function addReactiveVariables(node, list, reactiveFunctions) {
+  const isReactiveCall = node.init?.type === 'CallExpression' && reactiveFunctions.includes(node.init?.callee?.name);
+  if (isReactiveCall && node.id.type === 'Identifier') {
+    list.push(node.id.name);
+  }
+}
+
+/**
+ * 変数の初期値がreactiveFunctionsに含まれる関数から取得する場合、その変数名をリストに追加する関数
+ * @param {VariableDeclarator} node ASTのノード
+ * @param {string[]} list 変数名リスト
+ * @param {string[]} reactiveFunctions リアクティブな関数名のリスト
+ * @returns {void}
+ */
+function addToVariablesListFromCalleeWithArgument(node, list, reactiveFunctions) {
+  const isReactiveCall = node.init?.type === 'CallExpression' && reactiveFunctions.includes(node.init?.callee?.name);
+  if (isReactiveCall && node.id.type === 'ObjectPattern') {
+    list.push(...node.id.properties.map((property) => property.value.name).filter(Boolean));
+  }
+}
+
+/** @type {RuleModule} */
 export const reactiveValueSuffix = {
   meta: {
     type: 'suggestion',
     docs: {
-      description: 'リアクティブな値には ".value" をつける',
-      category: 'Best Practices',
-      recommended: false,
+      description: 'リアクティブな値に対して.valueでアクセスすることを強制する',
+      recommended: true,
     },
     fixable: 'code',
     messages: {
-      requireValueSuffix: 'リアクティブな値 "{{name}}" には ".value" が必要です。"{{name}}.value" に修正してください。',
+      requireValueSuffix: 'リアクティブな値 "{{name}}" には "{{name}}.value" でアクセスする必要があります。',
     },
     schema: [],
   },
   create(context) {
-    // リアクティブ変数のスタックを管理する配列
-    let reactiveVariablesStack = [];
+    const reactiveFunctions = ['toRefs', 'storeToRefs', 'computed', 'ref', 'reactive'];
+    const variableFromReactiveFunctions = [];
+    const functionArguments = [];
 
     /**
-     * スコープの開始時に呼び出され、新しいセットをスタックにプッシュする
+     * VariableDeclaratorノードをチェックする関数
+     * @param {VariableDeclarator} node
      */
-    function enterScope() {
-      reactiveVariablesStack.push(new Set());
+    function checkVariableDeclarator(node) {
+      addReactiveVariables(node, variableFromReactiveFunctions, reactiveFunctions);
+      addToVariablesListFromCalleeWithArgument(node, variableFromReactiveFunctions, reactiveFunctions);
     }
 
     /**
-     * スコープの終了時に呼び出され、スタックからセットをポップする
+     * FunctionDeclarationノードをチェックする関数
+     * @param {FunctionDeclaration} node
      */
-    function exitScope() {
-      reactiveVariablesStack.pop();
+    function checkFunctionDeclaration(node) {
+      addArgumentsToList(node, functionArguments);
     }
 
     /**
-     * 現在のスコープで管理されているリアクティブ変数のセットを返す
-     * @returns {Set} 現在のスコープのリアクティブ変数セット
+     * ArrowFunctionExpressionノードをチェックする関数
+     * @param {ArrowFunctionExpression} node
      */
-    function currentReactiveVariables() {
-      return reactiveVariablesStack[reactiveVariablesStack.length - 1];
+    function checkArrowFunctionExpression(node) {
+      addArgumentsToList(node, functionArguments);
     }
 
     /**
-     * 変数がリアクティブ関数の呼び出しで初期化されているかを確認する
-     * @param {ASTNode} node - チェックするASTノード
-     * @returns {boolean} - リアクティブ関数の呼び出しで初期化されている場合はtrue
+     * Identifierノードをチェックする関数
+     * @param {Identifier} node
      */
-    function isReactiveCallExpression(node) {
-      return (
-        node.init &&
-        node.init.type === 'CallExpression' &&
-        node.init.callee.type === 'Identifier' &&
-        reactiveFunctions.includes(node.init.callee.name)
-      );
-    }
+    function checkIdentifier(node) {
+      const parent = /** @type {any} */ (node.parent);
+      const parentType = parent?.type;
 
-    /**
-     * リアクティブ変数をセットに追加する
-     * @param {ASTNode} node - 追加するASTノード
-     */
-    function addReactiveVariables(node) {
-      const reactiveVariables = currentReactiveVariables();
-      if (node.id.type === 'ObjectPattern') {
-        node.id.properties.forEach((property) => {
-          if (property.value.type === 'Identifier') {
-            reactiveVariables.add(property.value.name);
-          }
+      // ノードが.valueではないかを確認
+      const parentPropertyWithoutValue = parent?.property?.name !== 'value';
+
+      // ノードが関数の引数に含まれないかを確認
+      const isNotFunctionArguments = !functionArguments.includes(node.name);
+
+      // ノードが宣言された変数ではないかを確認
+      const isNotOriginalDeclaredVariable = parentType !== 'VariableDeclarator' && parentType !== 'MemberExpression';
+
+      // ノードがオブジェクトのキーではないかを確認
+      const isNotObjectKey = !(parentType === 'Property' && parent.key.name === node.name);
+
+      // ノードが宣言の一部ではないかを確認
+      const isNotOriginalDeclaration = parentType !== 'MemberExpression' && parentType !== 'Property';
+
+      // ノードがwatch関数の引数ではないかを確認
+      let isWatchArguments = false;
+      let ancestor = /** @type {any} */ (parent);
+      while (ancestor && ancestor.type !== 'CallExpression') {
+        ancestor = /** @type {any} */ (ancestor.parent);
+      }
+
+      const isWatch = ancestor?.callee?.name === 'watch';
+      if (isWatch) {
+        const isNormalWatchArguments =
+          parentType === 'CallExpression' && parent.callee?.name === 'watch' && ancestor.arguments?.indexOf(node) === 0;
+        const isArrayWatchArguments =
+          parentType === 'ArrayExpression' &&
+          parent.parent?.type === 'CallExpression' &&
+          parent.parent?.callee?.name === 'watch';
+        isWatchArguments = isNormalWatchArguments || isArrayWatchArguments;
+      }
+
+      // 条件をすべて満たす場合に警告を報告
+      if (
+        isNotOriginalDeclaredVariable &&
+        isNotObjectKey &&
+        !isWatchArguments &&
+        isNotFunctionArguments &&
+        parentPropertyWithoutValue &&
+        isNotOriginalDeclaration &&
+        variableFromReactiveFunctions.includes(node.name)
+      ) {
+        context.report({
+          node,
+          messageId: 'requireValueSuffix',
+          data: { name: node.name },
         });
-      } else if (node.id.type === 'Identifier') {
-        reactiveVariables.add(node.id.name);
       }
     }
 
     /**
-     * リアクティブ変数が直接使用されているかを確認する
-     * @param {ASTNode} node - チェックするASTノード
-     * @returns {boolean} - 直接使用されている場合はtrue
+     * MemberExpressionノードをチェックする関数
+     * @param {MemberExpression} node
      */
-    function isDirectReactiveUsage(node) {
-      const reactiveVariables = currentReactiveVariables();
-      return (
-        node.object.type === 'Identifier' &&
-        reactiveVariables.has(node.object.name) &&
-        node.property.name !== 'value' &&
-        !(node.parent && node.parent.type === 'MemberExpression' && node.parent.property.name === 'value') &&
-        !(node.parent && (node.parent.type === 'VariableDeclarator' || node.parent.type === 'Property')) &&
-        !(isWatchCallExpression(node.parent) && node.parent.arguments[0] === node)
-      );
-    }
-
-    /**
-     * リアクティブ変数が識別子として直接使用されているかを確認する
-     * @param {ASTNode} node - チェックするASTノード
-     * @returns {boolean} - 直接使用されている場合はtrue
-     */
-    function isReactiveIdentifier(node) {
-      const reactiveVariables = currentReactiveVariables();
-      return (
-        reactiveVariables.has(node.name) &&
-        !(node.parent && node.parent.type === 'MemberExpression' && node.parent.property.name === 'value') &&
-        !(node.parent && (node.parent.type === 'VariableDeclarator' || node.parent.type === 'Property')) &&
-        !(isWatchCallExpression(node.parent) && node.parent.arguments[0] === node)
-      );
-    }
-
-    function isWatchCallExpression(node) {
-      return (
-        node && node.type === 'CallExpression' && node.callee.type === 'Identifier' && node.callee.name === 'watch'
-      );
+    function checkMemberExpression(node) {
+      const propertyWithoutValue = node.property?.name !== 'value';
+      if (propertyWithoutValue && variableFromReactiveFunctions.includes(node.object?.name)) {
+        context.report({
+          node,
+          messageId: 'requireValueSuffix',
+          data: { name: node.object.name },
+        });
+      }
     }
 
     return {
-      // プログラム（グローバルスコープ）の開始と終了時にスコープを管理
-      Program() {
-        enterScope();
-      },
-      'Program:exit'() {
-        exitScope();
-      },
-      // 関数スコープの開始と終了時にスコープを管理
-      FunctionDeclaration() {
-        enterScope();
-      },
-      'FunctionDeclaration:exit'() {
-        exitScope();
-      },
-      // ブロックスコープの開始と終了時にスコープを管理
-      BlockStatement() {
-        enterScope();
-      },
-      'BlockStatement:exit'() {
-        exitScope();
-      },
-      // リアクティブ変数の宣言を検出してセットに追加
-      VariableDeclarator(node) {
-        if (isReactiveCallExpression(node)) {
-          addReactiveVariables(node);
-        }
-      },
-      // リアクティブ変数が直接使用されている場合にエラーを報告
-      MemberExpression(node) {
-        if (isDirectReactiveUsage(node)) {
-          context.report({
-            node: node.property,
-            messageId: 'requireValueSuffix',
-            data: {
-              name: node.object.name,
-            },
-            // NOTE: 自動修正を有効にする場合はコメントアウトを解除してください
-            // fix: (fixer) => {
-            //   return fixer.insertTextAfter(node, '.value');
-            // },
-          });
-        }
-      },
-      // リアクティブ変数が識別子として直接使用されている場合にエラーを報告
-      Identifier(node) {
-        if (isReactiveIdentifier(node)) {
-          context.report({
-            node: node,
-            messageId: 'requireValueSuffix',
-            data: {
-              name: node.name,
-            },
-            // NOTE: 自動修正を有効にする場合はコメントアウトを解除してください
-            // fix: (fixer) => {
-            //   return fixer.insertTextAfter(node, '.value');
-            // },
-          });
-        }
-      },
+      VariableDeclarator: checkVariableDeclarator,
+      FunctionDeclaration: checkFunctionDeclaration,
+      ArrowFunctionExpression: checkArrowFunctionExpression,
+      Identifier: checkIdentifier,
+      MemberExpression: checkMemberExpression,
     };
   },
 };
