a7a8757254a042ac6ad807cb574d503ded78d11f
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-07-10 20:18:34 +0900

    :fire: delete: 不要なファイルの削除

--- settings/utils/constant.ts
+++ settings/utils/constant.ts
@@ -8,7 +8,6 @@ export const PROJECT_ROOT = path.join(__dirname, '../../');
 export const STORE_DIR = path.join(PROJECT_ROOT, 'src/store');
 
 export const STORE_DATA_DIR = path.join(PROJECT_ROOT, 'settings/data');
-export const STORE_FILE_LIST_PATH = path.join(STORE_DATA_DIR, 'store-file-list.json');
 export const STORE_STATE_LIST_PATH = path.join(STORE_DATA_DIR, 'store-state-list.json');
 export const STORE_GETTERS_LIST_PATH = path.join(STORE_DATA_DIR, 'store-getters-list.json');
 
