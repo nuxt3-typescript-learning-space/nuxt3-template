912ff9e14ec80e13197921aca7547eae258954d1
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-07-13 12:00:07 +0900

    :memo: docs: 補足のコメントを追加

--- settings/rules/utils/constant.js
+++ settings/rules/utils/constant.js
@@ -13,6 +13,8 @@ export const COMPOSABLE_FUNCTION_PATTERN = /^use[A-Z]/;
  * .valueチェックをスキップする関数の名前を格納するリスト
  *
  * このリストに含まれる関数名の引数は`.value`チェックがスキップされます。
+ *
+ * 本来であれば`settings/data/json`に記述するべきですが、JSDocも書けるためこちらに記述しています。
  * @type {string[]}
  */
 export const SKIP_CHECK_ARGUMENT_FUNCTION_NAMES = [];
