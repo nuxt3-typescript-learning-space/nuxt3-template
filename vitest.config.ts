e6a807707b4c59c2d9cb436ef6b3f909353eded5
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-07-14 16:43:41 +0900

    :wrench: chore: テストのcoverageに関する設定を追加

--- vitest.config.ts
+++ vitest.config.ts
@@ -2,6 +2,13 @@ import { defineVitestConfig } from '@nuxt/test-utils/config';
 
 export default defineVitestConfig({
   test: {
+    globals: true,
     environment: 'nuxt',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: '../log/coverage',
+      all: true,
+    },
   },
 });
