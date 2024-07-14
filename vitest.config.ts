b7bd03dd65982eb12e16dfc57c42a66ec848928f
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-07-14 15:37:47 +0900

    :wrench: chore: coverageの設定

--- vitest.config.ts
+++ vitest.config.ts
@@ -4,5 +4,11 @@ export default defineVitestConfig({
   test: {
     globals: true,
     environment: 'nuxt',
+    coverage: {
+      provider: 'v8',
+      reporter: ['text', 'json', 'html'],
+      reportsDirectory: '../log/coverage',
+      all: true,
+    },
   },
 });
