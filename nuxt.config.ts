b2d3f5cd33c44d483b266f6b986b600cb09d4186
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-07-30 23:27:46 +0900

    :wrench: chore: GTM組み込み対応

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,24 @@ export default defineNuxtConfig({
   devtools: { enabled: true },
   srcDir: 'src/',
   ssr: true,
-  modules: ['@nuxt/eslint', '@nuxtjs/tailwindcss', 'shadcn-nuxt', '@pinia/nuxt', '@nuxt/test-utils/module'],
+  modules: [
+    '@nuxt/eslint',
+    '@nuxtjs/tailwindcss',
+    'shadcn-nuxt',
+    '@pinia/nuxt',
+    '@nuxt/test-utils/module',
+    '@zadigetvoltaire/nuxt-gtm',
+  ],
   shadcn: {
     prefix: '',
     componentDir: './src/components/ui/',
+  },
+  runtimeConfig: {
+    public: {
+      gtm: {
+        id: process.env.NUXT_PUBLIC_GTM_ID || '',
+      },
+    },
   },
   pinia: {
     storesDirs: ['./src/store'],
