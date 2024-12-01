9e305a8faf1cec5fa7bc222ec8eb9e3c4f30e1e0
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2024-12-01 21:30:26 +0900

    :wrench: chore: vue-eslint-parser設定

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,11 +3,13 @@ import globals from 'globals';
 import withNuxt from './.nuxt/eslint.config.mjs';
 import eslintCustomRulesPlugin from './settings/rules/index.js';
 import eslintReactiveValueSuffix from 'eslint-plugin-reactive-value-suffix';
+import vueEslintParser from 'vue-eslint-parser';
 
 export default withNuxt([
   {
     ignores: ['.cz-config.cts', 'prettier.config.mjs', 'eslint.config.mjs', 'settings/rules/**/*'],
     languageOptions: {
+      parser: vueEslintParser,
       parserOptions: {
         parser: '@typescript-eslint/parser',
         project: './tsconfig.json',
