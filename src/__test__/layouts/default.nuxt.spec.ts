2ef29748d8badbd1b9cbfd8daf1ca9aa209027bb
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-04-01 19:46:35 +0900

    :fire: delete: 不要なテストファイルの削除

--- src/__test__/layouts/default.nuxt.spec.ts
+++ src/__test__/layouts/default.nuxt.spec.ts
@@ -1,21 +1,15 @@
-import { afterEach, beforeEach, describe, expect, test, vi } from 'vitest';
-import { bindTestingPinia, mountSuspendedComponent } from '@/__test__/testHelper';
+import { afterEach, describe, expect, test, vi } from 'vitest';
+import { mountSuspendedComponent } from '@/helper/test';
 import DefaultLayout from '@/layouts/default.vue';
 
 describe('src/layouts/default.vue', () => {
-  let testingPinia: ReturnType<typeof bindTestingPinia>;
-
-  beforeEach(() => {
-    testingPinia = bindTestingPinia();
-  });
-
   afterEach(() => {
     vi.resetAllMocks();
     vi.restoreAllMocks();
   });
 
   test('コンポーネントが正しくレンダリングされるか', async () => {
-    const wrapper = await mountSuspendedComponent(DefaultLayout, testingPinia);
+    const wrapper = await mountSuspendedComponent(DefaultLayout);
     expect(wrapper.exists()).toBe(true);
     expect(wrapper.find('#nuxt-default-layout').exists()).toBe(true);
   });
@@ -24,7 +18,7 @@ describe('src/layouts/default.vue', () => {
     const slots = {
       default: () => h('div', { id: 'slot-test' }, [h('p', 'slot content')]),
     };
-    const wrapper = await mountSuspendedComponent(DefaultLayout, testingPinia, { slots });
+    const wrapper = await mountSuspendedComponent(DefaultLayout, { slots });
     expect(wrapper.find('#slot-test').exists()).toBe(true);
     expect(wrapper.find('p').text()).toBe('slot content');
   });
