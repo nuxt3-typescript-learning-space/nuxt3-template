a54d5859f4856f793971802a304855ae3086256c
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-04-02 10:35:53 +0900

    :fire: delete: 不要な定数削除

--- settings/data/utils/constant.ts
+++ settings/data/utils/constant.ts
@@ -1,10 +1,6 @@
 import path from 'path';
-import { fileURLToPath } from 'url';
 
-export const __filename = fileURLToPath(import.meta.url);
-export const __dirname = path.dirname(__filename);
-
-export const PROJECT_ROOT = path.join(__dirname, '../../../');
+export const PROJECT_ROOT = process.cwd();
 export const STORE_DIR = path.join(PROJECT_ROOT, 'src/store');
 
 export const STORE_DATA_DIR = path.join(PROJECT_ROOT, 'settings/data/json');
