33fc566c18040d7f6c4719ef0b258e8674271783
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-04-02 15:19:56 +0900

    :fire: delete: 不要な正規表現の削除

--- settings/data/utils/constant.ts
+++ settings/data/utils/constant.ts
@@ -7,8 +7,4 @@ export const STORE_DATA_DIR = path.join(PROJECT_ROOT, 'settings/data/json');
 export const STORE_STATE_LIST_PATH = path.join(STORE_DATA_DIR, 'store-state-list.json');
 export const STORE_GETTERS_LIST_PATH = path.join(STORE_DATA_DIR, 'store-getters-list.json');
 
-export const STATE_REGEX_PATTERN = [
-  /state\s*:\s*\(\)\s*:\s*\w+\s*=>\s*\(\s*{([\s\S]*?)}\s*\),/,
-  /state\s*:\s*\(\)\s*:\s*\w+\s*=>\s*\(\s*{([\s\S]*?)}\s*\)|state\s*:\s*\(\)\s*=>\s*\(\s*{([\s\S]*?)}\s*\)/,
-];
 export const GETTERS_REGEX_PATTERN = [/getters\s*:\s*{([\s\S]*?)}\s*(?=,\s*actions|$)/];
