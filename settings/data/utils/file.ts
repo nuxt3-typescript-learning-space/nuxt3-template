e80827f1d8ae07e9451b382e94bc7c979fb8d33d
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-04-02 15:41:38 +0900

    :adhesive_bandage: update: nodeプロトコル追加

--- settings/data/utils/file.ts
+++ settings/data/utils/file.ts
@@ -1,5 +1,5 @@
-import { existsSync, readFileSync, writeFileSync } from 'fs';
-import { join } from 'path';
+import { existsSync, readFileSync, writeFileSync } from 'node:fs';
+import { join } from 'node:path';
 import { format, resolveConfig } from 'prettier';
 import { PROJECT_ROOT } from './constant';
 
