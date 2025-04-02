38408b9de2106a2c8774f584accffc825c3bd205
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-04-02 15:37:09 +0900

    :recycle: refactor: 不要なラッパーを削除

--- settings/data/utils/parser.ts
+++ settings/data/utils/parser.ts
@@ -1,17 +1,6 @@
 import { readFile } from './file';
 import { createSourceFileFromContent } from './parser/astUtils';
 import { findDefineStoreCallsAndExtractStateProperties } from './parser/state';
-import type { SourceFile } from 'typescript';
-
-/**
- * SourceFileからstateプロパティを抽出する純粋関数
- *
- * @param sourceFile ステートプロパティを抽出するソースファイル
- * @returns 抽出されたステートプロパティ名の配列
- */
-const extractStatePropertiesFromSourceFile = (sourceFile: SourceFile): string[] => {
-  return findDefineStoreCallsAndExtractStateProperties(sourceFile);
-};
 
 /**
  * 指定されたファイルからstateプロパティを抽出するエントリ関数
@@ -22,5 +11,5 @@ const extractStatePropertiesFromSourceFile = (sourceFile: SourceFile): string[] 
 export const extractStateProperties = (filePath: string): string[] => {
   const fileContent = readFile(filePath);
   const sourceFile = createSourceFileFromContent(filePath, fileContent);
-  return extractStatePropertiesFromSourceFile(sourceFile);
+  return findDefineStoreCallsAndExtractStateProperties(sourceFile);
 };
