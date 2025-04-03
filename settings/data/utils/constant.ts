61f61260a7a49f29dd5569345a60412026aad0c0
XeicuLy <106128913+XeicuLy@users.noreply.github.com>
2025-04-03 22:00:18 +0900

    :fire: delete: getters取得処理刷新に伴い、不要な処理削除

--- settings/data/utils/constant.ts
+++ settings/data/utils/constant.ts
@@ -10,5 +10,3 @@ export const STORE_DIR = path.join(PROJECT_ROOT, 'src/store');
 export const STORE_DATA_DIR = path.join(PROJECT_ROOT, 'settings/data/json');
 export const STORE_STATE_LIST_PATH = path.join(STORE_DATA_DIR, 'store-state-list.json');
 export const STORE_GETTERS_LIST_PATH = path.join(STORE_DATA_DIR, 'store-getters-list.json');
-
-export const GETTERS_REGEX_PATTERN = [/getters\s*:\s*{([\s\S]*?)}\s*(?=,\s*actions|$)/];
--- settings/data/utils/logger.ts
+++ settings/data/utils/logger.ts
@@ -1,21 +1,4 @@
 /**
- * ログ出力関数
- * @param {string} message - ログメッセージ
- */
-export const logMessage = (message: string) => {
-  console.log(message); // eslint-disable-line no-console
-};
-
-/**
- * 共通のエラーハンドリング関数
- * @param {string} message - エラーメッセージ
- * @param {unknown} error - 発生したエラー
- */
-export const handleError = (message: string, error: unknown) => {
-  console.error(`${message}\n詳細:`, error); // eslint-disable-line no-console
-};
-
-/**
  * キャッチしたエラーをWarnログとして出力する関数（開発環境用）
  * @param message - ログに出すメッセージ
  * @param error - catchしたerrorオブジェクト
